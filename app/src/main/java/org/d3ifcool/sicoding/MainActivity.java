79e0a833f15c9d7cd987c27d126f0cacc0a76504
Richofedhias <Richofedhias@users.noreply.github.com>
2019-09-05 15:29:34 +0700

    menambahkan TODO

--- app/src/main/java/org/d3ifcool/sicoding/MainActivity.java
+++ app/src/main/java/org/d3ifcool/sicoding/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //TODO: mengerjakan fitur 1
+        //TODO : meengerjakan fitur 2
     }
 }
