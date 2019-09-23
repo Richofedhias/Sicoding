4a84cd85cf5340fcde670d5af0ee745e36dbad4d
milzam6706170099 <avenged.sevencold.mm@gmail.com>
2019-09-23 23:44:18 +0700

    membuat fragment home dan fragment profile

--- app/src/main/java/org/d3ifcool/sicoding/beranda/BerandaActivity.java
+++ app/src/main/java/org/d3ifcool/sicoding/beranda/BerandaActivity.java
@@ -1,21 +1,32 @@
 package org.d3ifcool.sicoding.beranda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.d3ifcool.sicoding.R;
 
 import java.util.ArrayList;
 
 public class BerandaActivity extends AppCompatActivity {
-    ViewFlipper vF_slideShow;
-    RecyclerView rV_list;
+    BottomNavigationView navView;
+
     BerandaAdapter adapter;
     ArrayList<IsiBeranda> list = new ArrayList<>();
 
@@ -23,37 +34,22 @@ public class BerandaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
-        vF_slideShow = findViewById(R.id.vF_slideshow);
-        rV_list = findViewById(R.id.rV_listButton);
+        navView = findViewById(R.id.bottom_nav);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navView, navController);
 
-        //SlideShow
-        int images[] = {R.drawable.slide_1, R.drawable.slide_2, R.drawable.slide_3};
-        for (int i = 0; i < images.length; i++) {
-            slideShow(images[i]);
-        }
+
 
         //Button Bahasa Pemrograman
-        rV_list.setHasFixedSize(true);
-        RecyclerView.LayoutManager lm = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        rV_list.setLayoutManager(lm);
-        adapter = new BerandaAdapter(list);
-        rV_list.setAdapter(adapter);
 
-        list.add(new IsiBeranda("Boostrap",R.drawable.logo));
-        list.add(new IsiBeranda("Boostrap",R.drawable.logo));
-        list.add(new IsiBeranda("Boostrap",R.drawable.logo));
-        list.add(new IsiBeranda("Boostrap",R.drawable.logo));
 
     }
 
-    private void slideShow(int image) {
-        ImageView imageView = new ImageView(this);
-        imageView.setBackgroundResource(image);
-        vF_slideShow.addView(imageView);
-        vF_slideShow.setFlipInterval(4000);
-        vF_slideShow.setAutoStart(true);
 
-        vF_slideShow.setInAnimation(this,android.R.anim.slide_in_left);
-        vF_slideShow.setOutAnimation(this,android.R.anim.slide_out_right);
-    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.bottom_nav_menu, menu);
+//        return true;
+//    }
 }
