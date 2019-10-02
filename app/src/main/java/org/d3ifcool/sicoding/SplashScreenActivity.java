3cc75c1d33ccd660d6acb9d502a16aaae4ecfa1b
erbina6706174043 <48651444+erbina6706174043@users.noreply.github.com>
2019-10-02 21:33:51 +0700

    Meng-update Layout splash screen

--- app/src/main/java/org/d3ifcool/sicoding/SplashScreenActivity.java
+++ app/src/main/java/org/d3ifcool/sicoding/SplashScreenActivity.java
@@ -4,21 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import org.d3ifcool.sicoding.login.LoginActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    ImageView top;
+    Animation fromtop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        // langsung pindah ke MainActivity
-        // begitu memasuki splash screen ini
-        Intent intent = new Intent(this, WalkthroughActivity.class);
-        startActivity(intent);
-        finish();
+        top = (ImageView) findViewById(R.id.iV_splash);
+        fromtop = AnimationUtils.loadAnimation(this, R.anim.from_top);
 
+        top.setAnimation(fromtop);
     }
 }
